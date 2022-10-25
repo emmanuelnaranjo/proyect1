48370649326ac3e22d6d64489e142d7cb9af14a4
Emmanuel <emmanuelnaranjo.xyz@gmail.com>
2022-10-25 18:55:40 -0400

    just cahngin to objects

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
         let generator ={};
-        generator.quotesList=["Start each day with positive tought and a grateful heart. Roy Bennet","the way i see if you want to see the rainbow, you gotta put up with rain. Dollin Parton","Only in the darkness you can see stars. Mather Luther King","intellectual growth should comence at birth and cease only at death"];
+        function quote (content ,author)
+        {
+            this.content = content;
+            this.content= author;
+        }
+
+        
+        let quote1 = new quote("Start each day with positive tought and a grateful heart.","Roy Bennet");
+        let quote2 = new quote("the way i see if you want to see the rainbow, you gotta put up with rain."," Dollin Parton");
+        let quote3 = new quote("Only in the darkness you can see stars.","Mather Luther King"); 
+        let quote4 = new quote("intellectual growth should comence at birth and cease only at death","Albert Einstein")   
+        generator.quotesList=[quote1,quote2,quote3,quote4];
         generator.quote = function () 
         {
             let randomNum = Math.random();
